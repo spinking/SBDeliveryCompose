20159e5413fab64ad6a142e6204f885d94cb0b7e
Serge Bashkatov <spinking@mail.ru>
2021-12-10 17:31:22 +0400

    added MenuScreen

--- app/src/main/java/ru/skillbranch/sbdelivery/screens/menu/ui/MenuScreen.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/screens/menu/ui/MenuScreen.kt
@@ -1,9 +1,17 @@
 package ru.skillbranch.sbdelivery.screens.menu.ui
 
+import android.util.Log
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
+import ru.skillbranch.sbdelivery.screens.components.LazyGrid
 import ru.skillbranch.sbdelivery.screens.menu.logic.MenuFeature
 import ru.skillbranch.sbdelivery.screens.root.logic.Msg
 
@@ -13,6 +21,44 @@ import ru.skillbranch.sbdelivery.screens.root.logic.Msg
 @Composable
 fun MenuScreen(state: MenuFeature.State, accept: (Msg) -> Unit) {
 
+    val dispatcher = LocalOnBackPressedDispatcherOwner.current!!.onBackPressedDispatcher
+    val backCallback = remember {
+        object : OnBackPressedCallback(false) {
+            override fun handleOnBackPressed() {
+                Log.e("MenuScreen", "press back")
+                MenuFeature.Msg.PopCategory
+                    .let(Msg::Menu)
+                    .also(accept)
+            }
+        }
+    }
+
+    SideEffect {
+        Log.e("MenuScreen", "side effect")
+        backCallback.isEnabled = state.parent != null
+    }
+
+    DisposableEffect(key1 = dispatcher) {
+        Log.e("MenuScreen", "add back callback")
+        dispatcher.addCallback(backCallback)
+
+        onDispose {
+            Log.e("MenuScreen", "remove back callback")
+            backCallback.remove()
+        }
+    }
+
+    LazyGrid(
+        items = state.current,
+        cols = 3,
+        cellsPadding = 16.dp
+    ) { category ->
+        MenuItem(item = category, onClick = {
+            MenuFeature.Msg.ClickCategory(category.id, category.title)
+                .let(Msg::Menu)
+                .also(accept)
+        })
+    }
 }
 
 
